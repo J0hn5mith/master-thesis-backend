9ecea3fad2cfbb08a2bfaee1abbf669098ef57d3
Jan Meier <janmeier0.o@gmail.com>
2017-01-10 14:08:35 +0100

    started with home page

--- web/master_thesis_backend/urls.py
+++ web/master_thesis_backend/urls.py
@@ -20,6 +20,6 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='base.html')),
+    url(r'^$', TemplateView.as_view(template_name='home.html')),
     url(r'^admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
