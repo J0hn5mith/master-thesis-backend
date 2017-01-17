2e7f00f19d21597409c4b202fe4ccbcf19c2b8cc
Jan Meier <janmeier0.o@gmail.com>
2017-01-17 12:24:27 +0100

    registration complete (without dialog for further user details)

--- web/home/urls.py
+++ web/home/urls.py
@@ -2,7 +2,28 @@ from django.views.generic import TemplateView
 from django.conf.urls import url
 
 urlpatterns = [
-        url(r'^$', TemplateView.as_view(template_name='home.html')),
-        url(r'^login$', TemplateView.as_view(template_name='login.html')),
-        url(r'^register$', TemplateView.as_view(template_name='register.html')),
+        url(r'^$', TemplateView.as_view(template_name='home.html'), name="home"),
+        url(
+            r'^login$',
+            TemplateView.as_view(template_name='login.html'),
+            name='login'
+
+            ),
+        url(
+            r'^register$', 
+            TemplateView.as_view(template_name='register.html'),
+            name="register"
+            ),
+        url(
+            r'^technology$',
+            TemplateView.as_view(template_name='technology.html'),
+            name='technology'
+            ),
+        url(
+            r'^support$',
+            TemplateView.as_view(template_name='support.html'),
+            name='support'
+            ),
+        url(r'^mail-test$',
+            TemplateView.as_view(template_name='registration/activation_email.html')),
         ]
