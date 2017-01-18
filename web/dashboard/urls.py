285c92eedf6fc6bc42f7b852767692cac3011f2f
Jan Meier <janmeier0.o@gmail.com>
2017-01-18 14:32:07 +0100

    added dashboard navbar

--- web/dashboard/urls.py
+++ web/dashboard/urls.py
@@ -5,13 +5,21 @@ urlpatterns = [
         url(
             r'^$',
             TemplateView.as_view(template_name='dashboard.html'),
-            name="dashboard"
+            name='dashboard'
             ),
-        url(r'tags$', TemplateView.as_view(template_name='tags.html')),
         url(
-            r'^notifications$', 
-            TemplateView.as_view(template_name='notifications.html'),
-            name="notifications"
+            r'tags$',
+            TemplateView.as_view(template_name='tags.html'),
+            name='tags'
             ),
-        url(r'^settings$', TemplateView.as_view(template_name='settings.html')),
+        url(
+            r'^notifications$',
+            TemplateView.as_view(template_name='notifications.html'),
+            name='notifications'
+            ),
+        url(
+            r'^settings$',
+            TemplateView.as_view(template_name='settings.html'),
+            name='settings'
+            ),
         ]
