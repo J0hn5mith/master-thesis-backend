5d5ffa98c551eefd2953327214fe3ff190e76fb4
Jan Meier <janmeier0.o@gmail.com>
2017-02-23 15:01:44 +0100

    some first tag properties, tag overview looks as it should

--- web/tags/admin.py
+++ web/tags/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from tags.models import Tag
 
 class TagAdmin(admin.ModelAdmin):
-    list_display = ('name', 'uid', 'user_with_avatar')
+    list_display = ('name', 'uid', 'user_with_avatar', 'last_update', 'active')
     pass
 
 admin.site.register(Tag, TagAdmin)
--- web/tags/models.py
+++ web/tags/models.py
@@ -1,8 +1,9 @@
-from django.db import models
 from django.conf import settings
 from django.contrib.auth import get_user_model
-from django.utils.html import format_html
 from django.contrib.staticfiles.templatetags.staticfiles import static
+from django.db import models
+from django.utils.html import format_html
+from django.utils.translation import ugettext as _
 
 
 class Tag(models.Model):
@@ -31,6 +32,24 @@ class Tag(models.Model):
         blank=True,
         null=True,
     )
+
+    active = models.BooleanField(default=True)
+
+    last_update = models.DateTimeField(
+        null=True,
+        blank=True,
+    )
+
+    # Null values mean, that the charge status is unknown
+    charge_status = models.DecimalField(
+        null=True,
+        max_digits=3,
+        decimal_places=3,
+        blank=True,
+    )
+
+    def get_status(self):
+        return 0
 
     def user_with_avatar(self):
         return format_html(
