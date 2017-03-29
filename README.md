596609b111c785a7e76831f94915c87ab126cad3
Jan Meier <janmeier0.o@gmail.com>
2017-03-29 13:04:07 +0200

    fixed login issue

--- README.md
+++ README.md
@@ -13,3 +13,10 @@
 * Create a user `postgres` with the password `postgres` and grant him all rights for the `postgres` database
 
 *(Hint: This project is based on this [tutorial](https://realpython.com/blog/python/django-development-with-docker-compose-and-machine/).)*
+
+
+FAQ:
+* How can I execute commands for the single application components?
+    * First attach yourself to the running docker image `docker exec -it <image_name> bash`
+    * Then you get a command line which is located inside the running docker iamge.
+    * Attention: The linux distributions are reduced to a minimum and some commonly available tools are not installed!
--- web/master_thesis_backend/settings/dev.py
+++ web/master_thesis_backend/settings/dev.py
@@ -3,35 +3,22 @@ from .base import *
 
 DEBUG = True
 
-INSTALLED_APPS += ('debug_toolbar', 'django_extensions', )
-
 INTERNAL_IPS = ('127.0.0.1', )
 # 'localhos' makes sure that non docker setup works
 ALLOWED_HOSTS = (os.environ.get('HOST'), 'localhost')
 PAGE_URL = os.environ.get('PAGE_URL')
 
-#: Don't send emails, just print them on stdout
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_PORT = os.environ.get('EMAIL_PORT')
 EMAIL_USE_TLS = True
-EMAIL_HOST = os.environ.get('EMAIL_HOST')
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
-SECRET_KEY = os.environ.get(
-    'SECRET_KEY',
-    '3vdexi69&m+lus9b0#wx57m%@6qz8rb^t=oogxt&6wcw$z*!zn',
-)
-SECRET_KEY = '3vdexi69&m+lus9b0#wx57m%@6qz8rb^t=oogxt&6wcw$z*!zn',
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# Special test settings
-if 'test' in sys.argv:
-    PASSWORD_HASHERS = (
-        'django.contrib.auth.hashers.SHA1PasswordHasher',
-        'django.contrib.auth.hashers.MD5PasswordHasher',
-    )
+INSTALLED_APPS += ('debug_toolbar', 'django_extensions', )
 
-    LOGGING['root']['handlers'] = []
 
 ##################################################
 # Third Party
@@ -52,10 +39,3 @@ SENDSMS_TWILIO_AUTH_TOKEN = TWILIO_AUTH_TOKEN
 # Celery
 CELERY_ALWAYS_EAGER = True
 CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
-
-#TODO: Remove
-RAVEN_CONFIG = {
-    'dsn': os.environ.get('SENTRY_URL'),
-    # 'release': raven.fetch_git_sha(os.path.dirname(os.pardir)),
-    'release': 'dev',
-}
